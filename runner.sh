bd4c377f75032bd033946f2dc329388a9568c5e1
Kacper Żuk <kacper.b.zuk@gmail.com>
2018-07-13 16:16:00 +0000

    nginx-proxy compat

--- runner.sh
+++ runner.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+function nginx_proxy_compat {
+    if [ ! -e /etc/letsencrypt/live ]; then
+        return
+    fi
+
+    for dpath in /etc/letsencrypt/live/*; do
+        domain=$(basename "$dpath")
+        mkdir -p /etc/letsencrypt/nginx-proxy/
+        if [ ! -e /etc/letsencrypt/nginx-proxy/$domain.crt ]; then
+            ln -s $dpath/fullchain.pem /etc/letsencrypt/nginx-proxy/$domain.crt
+        fi
+        if [ ! -e /etc/letsencrypt/nginx-proxy/$domain.key ]; then
+            ln -s $dpath/privkey.pem /etc/letsencrypt/nginx-proxy/$domain.key
+        fi
+    done
+}
+
 set -x
+
 while true; do
     date
     certbot "$@"
+    nginx_proxy_compat
     sleep 24h
 done
